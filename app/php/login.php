37d4c6723d0c9843e54f70afffa5596b4912d43d
Jaya Kasa <kasa288@gmail.com>
2016-02-04 13:22:34 -0500

    ER Diagram for project has been added to docs
    Created a php folder to hold all php code to connect to server
    
    login.php
    contains variable for connecting to the DB
    
    dbconnect.php
    establishes the connection to the DB using variable from login.php
    
    index.php
    sample landing page for testing php code quickly
    DELETE THIS FILE WHEN DONE WITH EVERYTHING OR CHANGE ITS NAME TO SOMETHING ELSE TO AVOID MESSING WITH THE OVERALL PROJECT

---
+++ app/php/login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Jaya Kasa
+ * Date: 2/4/2016
+ * Time: 12:42 PM
+ *
+ * This file details login details for the mysql server.
+ * Any changes to host, database, username, or password should be done here.
+ *
+ */
+
+$db_hostname= "localhost";
+$db_database = "rutgerswebresponse";
+$db_username = "jkasa";
+$db_password = "jkasa";
+
+// file is being closed to ensure that it can never be displayed as tezt
+// if anyone tries to access the file from a web browser
+?>
