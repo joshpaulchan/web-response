ed44e524fe7556631d1eca7a832853b5af33c413
Jaya Kasa <kasa288@gmail.com>
2016-03-03 13:10:44 -0500

    returning messages in json format

--- app/php/calls/GetAllMessages.php
+++ app/php/calls/GetAllMessages.php
@@ -6,8 +6,8 @@
  * Time: 9:48 AM
  */
 
-require_once 'dbconnect.php';
-require_once 'queries/SelectQueries.php';
+require_once "../dbconnect.php";
+require_once '../queries/SelectQueries.php';
 
 /*
    * Collect all Details from Angular HTTP Request.
@@ -18,7 +18,29 @@ if(!$result = $db_server->query($allMessages)){
     die('There was an error running the query [ ' . $db_server->error . ' ].');
 }
 
+$json_result = array();
+//$return_arr = array();
+
+//using assoc here because it would be easier for future modifications
+while($row = $result->fetch_assoc()){
+    /*
+    $row_array['entry#'] = $row['entry#'];
+    $row_array['name'] = $row['name'];
+    $row_array['email'] = $row['email'];
+    $row_array['subject'] = $row['subject'];
+    $row_array['comment'] = $row['comment'];
+    $row_array['redirect'] = $row['redirect'];
+    $row_array['browserinfo'] = $row['browserinfo'];
+    $row_array['topic'] = $row['topic'];
+    $row_array['date'] = $row['date'];
+
+    array_push($return_arr,$row_array);
+     */
+
+    $json_result[] = $row;
+}
+
 //returning the results of the query in json format
-echo json_encode($result); //this will go back under "data" of angular call.
+echo json_encode($json_result); //this will go back under "data" of angular call.
 $result->free();
 ?>
