bb8aa660d52f246a9f63fd1c1a764fb50e638684
Jaya Kasa <kasa288@gmail.com>
2016-03-09 14:08:02 -0500

    setup DB on AWS for testing usage

---
+++ app/php/db/ucmlogin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kasa2
+ * Date: 3/9/2016
+ * Time: 2:04 PM
+ */
+//TODO use this for when we have to use the actual DB on urwebsrv
+$db_hostname = "localhost";
+$db_database = "rutgerswebresponse";
+$db_username = "uradmin";
+$db_password = "ur3710adm";
+
+$db_database = "ucmwebresponse";
+
+// file is being closed to ensure that it can never be displayed as tezt
+// if anyone tries to access the file from a web browser
+?>
