3fa28c335a37c517abcb97fc2662884e254fefda
Jaya Kasa <kasa288@gmail.com>
2016-03-23 12:30:38 -0400

    working on email solutions.
    created a file that handles sending emails and it works without any issues
    
    need to figure out how to retreive emails in a similar fashion
    
    check TODO list

--- app/php/Calls/search/deleteMessage.php
+++ app/php/Calls/search/deleteMessage.php
@@ -10,7 +10,7 @@ require_once '../../Caller.php';
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-@$message_id = $message_id;
+@$message_id = $request->message_id;
 
 //returning the results of the query in json format
 echo deleteMessages($message_id); //this will go back under "data" of angular call.
---
+++ app/php/email/getEmails.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kasa2
+ * Date: 3/23/2016
+ * Time: 11:31 AM
+ */
+
+$ host -t a kasa288.example.com
+
+
+?>
