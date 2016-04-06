20c4cc83d2b467d43c0aad84d473f664947fa7b6
Jaya Kasa <kasa288@gmail.com>
2016-04-06 17:18:29 -0400

    working on dynamic pagination
    finished users
    
    working on messages
    need to modify the message query in GetAllMessages.php

--- app/php/calls/GetAll/GetAllMessages.php
+++ app/php/calls/GetAll/GetAllMessages.php
@@ -9,6 +9,22 @@
 require_once '../../queries/SelectQueries.php';
 require_once 'GetAllCallOldDB.php';
 
+require_once '../../paginate/Paginator.php';
+//TODO change this later to the new db
+//require_once '../../db/dbtestlocal.php';
+require_once '../../db/dbconnect.php';
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+@$group_num = $request->group_num;
+
+static $paginate = null;
+$query = "select count(*) from message";
+
+if($paginate == null){
+    $paginate = new Paginator($db_server, $query, 25, $allUsers);
+}
+
 //returning the results of the query in json format
-echo getAll($allMessages); //this will go back under "data" of angular call.
+echo getAll($paginate, $allMessages); //this will go back under "data" of angular call.
 ?>
--- app/php/calls/getall/GetAllCallLocalDB.php
+++ app/php/calls/getall/GetAllCallLocalDB.php
@@ -8,49 +8,15 @@
 
 
 /**
- * Method connects to old DB and returns all messages
+ * Method connects to local DB and returns all messages
  *
- * @param $query
- * @return string
+ * @param $paginate
+ * @param $group_num
+ * @return mixed
  */
-function getAll($query){
+function getAll($paginate, $group_num){
 
-    //require_once  "../../db/dbconnect.php";
-    require_once "../../db/dbtestlocal.php";
-
-    if(!$result = $db_server->query($query)){
-        //the query failed
-        die('There was an error running the query [ ' . $db_server->error . ' ].');
-    }
-
-    $json_result = array();
-    //$return_arr = array();
-
-    //using assoc here because it would be easier for future modifications
-    while($row = $result->fetch_assoc()){
-        /*
-        $row_array['entry#'] = $row['entry#'];
-        $row_array['name'] = $row['name'];
-        $row_array['email'] = $row['email'];
-        $row_array['subject'] = $row['subject'];
-        $row_array['comment'] = $row['comment'];
-        $row_array['redirect'] = $row['redirect'];
-        $row_array['browserinfo'] = $row['browserinfo'];
-        $row_array['topic'] = $row['topic'];
-        $row_array['date'] = $row['date'];
-
-        array_push($return_arr,$row_array);
-         */
-
-        $json_result[] = $row;
-    }
-
-    //returning the results of the query in json format
-    return json_encode($json_result); //this will go back under "data" of angular call.
-
-    //not sure if this is unreachable code
-    //leaving this here for now
-    $result->free();
+    return $paginate->loadData($group_num);
 
 }
 ?>
