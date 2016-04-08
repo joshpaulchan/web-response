038743f0f6133152ca679b6a6ba6eb65201f2917
Jaya Kasa <kasa288@gmail.com>
2016-04-08 16:27:21 -0400

    moved utf8ize into the utils folder

--- app/php/Calls/GetAll/GetAllCallOldDB.php
+++ app/php/Calls/GetAll/GetAllCallOldDB.php
@@ -6,27 +6,15 @@
  * Time: 1:35 PM
  */
 
-
 /**
  * Method connects to old DB and returns all messages
  *
  * @param $paginate
  * @param $group_num
- * @return mixed
+ * @return data in utf8 format
  */
-
- function utf8ize($d) {
-     if (is_array($d)) {
-         foreach ($d as $k => $v) {
-             $d[$k] = utf8ize($v);
-         }
-     } else if (is_string ($d)) {
-         return utf8_encode($d);
-     }
-     return $d;
- }
-
 function getAll($paginate, $group_num){
+    require_once "../../util/utf8ize.php";
 
     return json_encode(utf8ize($paginate->loadData($group_num)));
 
--- app/php/calls/GetAll/GetAllUsers.php
+++ app/php/calls/GetAll/GetAllUsers.php
@@ -9,7 +9,22 @@
 require_once '../../queries/SelectQueries.php';
 //require_once 'GetAllCallNewDB.php';
 require_once 'GetAllCallLocalDB.php';
+require_once '../../paginate/Paginator.php';
+//TODO change this later to the new db
+require_once '../../db/dbtestlocal.php';
+//require_once '../../db/dbconnect.php';
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+@$group_num = $request->group_num;
+
+static $paginate = null;
+$query = "select count(*) from user";
+
+if($paginate == null){
+    $paginate = new Paginator($db_server, $query, 5, $allUsers);
+}
 
 //returning the results of the query in json format
-echo getAll($allUsers); //this will go back under "data" of angular call.
+echo getAll($paginate, $group_num); //this will go back under "data" of angular call.
 ?>
