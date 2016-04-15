3d9987911986a4f5d9e3fcfb3aec55915aa09f4b
joshpaulchan <joshpaulchan@gmail.com>
2016-04-15 16:24:23 -0400

    Refactor message fetching to add threading and load responses

--- app/php/Calls/GetAll/GetAllCallOldDB.php
+++ app/php/Calls/GetAll/GetAllCallOldDB.php
@@ -15,20 +15,59 @@
  * @return mixed
  */
 
- function utf8ize($d) {
-     if (is_array($d)) {
-         foreach ($d as $k => $v) {
-             $d[$k] = utf8ize($v);
-         }
-     } else if (is_string ($d)) {
-         return utf8_encode($d);
-     }
-     return $d;
- }
+ // TODO: use server?
+
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
+}
+
+function getResponses($m) {
+    // $result = $db_server->query("SELECT * FROM `response` WHERE `message_id` IN (" . $m["message_id"] . ") ORDER BY `created` DESC");
+    // SELECT * FROM `response` WHERE `message_id` IN (158) ORDER BY `created` DESC
+    $resp = array();
+
+    //using assoc here because it would be easier for future modifications
+    // while($row = $result->fetch_assoc()){
+    //     // reformat row
+    //     $resp[] = $row;
+    // }
+    // $result->free();
+    return (array_map("reformatResponses", $resp));
+}
+
+function insertResponses($msg) {
+    $responses = getResponses($msg);
+    // insert original message
+    $msg["thread"] = array(
+        array(
+            "sentBy" => $msg["email"],
+            "sentTo" => "us",
+            "createdAt" => $msg["created"],
+            "content" => $msg["descr"]
+        )
+    );
+    // add fetched responses
+    $msg["thread"] = array_merge($msg["thread"], $responses);
+
+    return ($msg);
+};
 
 function getAll($paginate, $group_num){
+    // get messages
+    $messages = $paginate->loadData($group_num);
 
-    return json_encode(utf8ize($paginate->loadData($group_num)));
+    // add responses
+    $messages = array_map("insertResponses", $messages);
+
+    // return
+    return json_encode(utf8ize($messages));
 
 }
 ?>
