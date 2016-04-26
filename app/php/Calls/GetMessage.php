2afe085a7789b37be927e47724083598686094fc
joshpaulchan <joshpaulchan@gmail.com>
2016-04-26 17:17:29 -0400

    Implement single message retrieval

--- app/php/Calls/GetMessage.php
+++ app/php/Calls/GetMessage.php
@@ -8,36 +8,69 @@
    * Collect all Details from Angular HTTP Request.
 */
 
-$result = array(
-    "id" => 0,
-    "subject" => "Aerospace Engineering",
-    "createdAt" => "Jan 5, 2016",
-    "createdBy" => "khushbudarji@rocketmail.com",
-    "folder" => "inbox",
-    "status" => array(
-        "selected" => false,
-        "unread"  => false,
-        "repliedTo" => true
-    ),
-    "thread" => array(
-        array(
-            "email" => "khushbudarji@rocketmail.com",
-            "createdAt" => "Jan 5, 2016",
-            "content" => "Hello ,\r\nI want to get some information about course at your university .\r\n\r\nI'm from India and studying last semester of master degree in communication and system.\r\nSo , can I apply there for aerospace engineering course\r\nAnd can you please guide me some about it .\r\nI hope you will reply positively\r\n\r\nThank you\r\nGave a nice day\r\n\r\n"
-        ),
-        array(
-            "email" => "person@example.com",
-            "createdAt" => "later on 13 AM",
-            "content" => "Ah,\r\nyes."
-        )
-    )
-);
+require_once '../db/dbconnect.php';
 
-if ($result["id"] != $_GET["messageId"]) {
-    http_response_code(404);
-    die("Message Id not found");
-} else {
-    //returning the results of the query in json format
-    echo json_encode($result); //this will go back under "data" of angular call.
+// $postdata = file_get_contents("php://input");
+// $request = json_decode($postdata);
+// @$m_id = $request->messageId;
+
+$m_id = $_GET["messageId"];
+
+function getResponses($m) {
+    global $db_server;
+    $result = $db_server->query("SELECT * FROM `response` WHERE `message_id` IN (" . $m["message_id"] . ") ORDER BY `created` DESC");
+    // SELECT * FROM `response` WHERE `message_id` IN (158) ORDER BY `created` DESC
+    $resp = array();
+
+    while($row = $result->fetch_assoc()){
+        // reformat row
+        $row["sentBy"] = $row["user_id"];
+        $row["sentTo"] = $row["to"];
+        $row["createdAt"] = $row["created"];
+        $row["content"] = $row["descr"];
+        $resp[] = $row;
+    }
+    $result->free();
+    return ($resp);
 }
+
+function insertResponses($msg) {
+    $responses = getResponses($msg);
+    // insert original message
+    $msg["thread"] = array(
+        array(
+            "sentBy" => $msg["email"],
+            "sentTo" => "us",
+            "createdAt" => $msg["created"],
+            "content" => $msg["descr"]
+        )
+    );
+    // add fetched responses
+    $msg["thread"] = array_merge($msg["thread"], $responses);
+
+    return ($msg);
+};
+
+function getMessage($id){
+    require_once "../util/utf8ize.php";
+    global $db_server;
+
+    // get message
+    $result = $db_server->query("SELECT * FROM `message` WHERE `message_id` = " . $id );
+    $message = $result->fetch_assoc();
+    // TODO: count bc if null return error
+    // http_response_code(404);
+    // die("Message Id not found");
+
+    // add responses
+    $message = insertResponses($message);
+    $result->free();
+
+    // return
+    return json_encode(utf8ize($message));
+}
+
+echo getMessage($m_id); //this will go back under "data" of angular call.
+
+$db_server->close();
 ?>
